d3081fe3ccc0b9e60fe7ba9ebef6fa3689eb3373
Kazuma Ito <zarigani2x@gmail.com>
2023-05-28 17:50:33 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HandyGraphic
+
+[HandyGraphic](http://www.cc.kyoto-su.ac.jp/~ogihara/Hg/)を用いた開発を手助けする VSCode 拡張機能です。
+
+![name](./run.gif)
---
+++ src/extension.ts
@@ -0,0 +1,41 @@
+import * as vscode from "vscode";
+import * as path from "path";
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand("handy-graphic.run", () => {
+    const dir = path.dirname(
+      vscode.window.activeTextEditor?.document.fileName || ""
+    );
+    const filePath = vscode.window.activeTextEditor?.document.fileName || "";
+    const fileName = path.basename(filePath);
+    const ext = path.extname(filePath);
+    const outFileName = path.basename(filePath, ext);
+
+    // kill the app/HgDisplayer
+    const killerTerminal =
+      vscode.window.terminals.find((t) => t.name === "HandyGraphic Killer") ||
+      vscode.window.createTerminal("HandyGraphic Killer");
+    killerTerminal.sendText("killall HgDisplayer");
+
+    // run the HgDisplayer
+    const displayerTerminal =
+      vscode.window.terminals.find(
+        (t) => t.name === "HandyGraphic Displayer"
+      ) || vscode.window.createTerminal("HandyGraphic Displayer");
+    displayerTerminal.sendText(
+      `/Applications/HgDisplayer.app/Contents/MacOS/HgDisplayer`
+    );
+
+    // compile and run the app
+    const terminal =
+      vscode.window.terminals.find((t) => t.name === "HandyGraphic") ||
+      vscode.window.createTerminal("HandyGraphic");
+    terminal.sendText(
+      `cd "${dir}" && hgcc -Wall -o "${outFileName}" "./${fileName}" && "./${outFileName}"`
+    );
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}

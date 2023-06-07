6725dfbc91c91015f848dda78015157b050aa82d
Kazuma Ito <zarigani2x@gmail.com>
2023-06-07 09:35:13 +0900

    refactor: Refactor extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,54 @@
 import * as vscode from "vscode";
 import * as path from "path";
 
+const terminalNames = {
+  main: "HandyGraphic",
+  displayer: "HandyGraphic Displayer",
+  killer: "HandyGraphic Killer",
+} as const;
+
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand("handy-graphic.run", () => {
     const config = vscode.workspace.getConfiguration("handy-graphic");
 
-    const dir = path.dirname(
-      vscode.window.activeTextEditor?.document.fileName || ""
-    );
-    const filePath = vscode.window.activeTextEditor?.document.fileName || "";
-    const fileName = path.basename(filePath);
-    const ext = path.extname(filePath);
-    const outFileName = path.basename(filePath, ext);
+    const fileAbsPath = vscode.window.activeTextEditor?.document.fileName || "";
+
+    if (!fileAbsPath) {
+      vscode.window.showErrorMessage(
+        "アクティブなファイルが見つかりませんでした。"
+      );
+      return;
+    }
+
+    const dirAbsPath = path.dirname(fileAbsPath);
+    const fileName = path.basename(fileAbsPath);
+    const fileExt = path.extname(fileAbsPath);
+    const outFileName = path.basename(fileAbsPath, fileExt);
 
     // kill the app/HgDisplayer
     const killerTerminal =
-      vscode.window.terminals.find((t) => t.name === "HandyGraphic Killer") ||
-      vscode.window.createTerminal("HandyGraphic Killer");
+      vscode.window.terminals.find((t) => t.name === terminalNames.killer) ||
+      vscode.window.createTerminal(terminalNames.killer);
     killerTerminal.sendText("killall HgDisplayer");
 
     // run the HgDisplayer
     const displayerTerminal =
-      vscode.window.terminals.find(
-        (t) => t.name === "HandyGraphic Displayer"
-      ) || vscode.window.createTerminal("HandyGraphic Displayer");
+      vscode.window.terminals.find((t) => t.name === terminalNames.displayer) ||
+      vscode.window.createTerminal(terminalNames.displayer);
     displayerTerminal.sendText(
       `/Applications/HgDisplayer.app/Contents/MacOS/HgDisplayer`
     );
 
     // compile and run the app
-    const terminal =
-      vscode.window.terminals.find((t) => t.name === "HandyGraphic") ||
-      vscode.window.createTerminal("HandyGraphic");
-    terminal.sendText(
-      `cd "${dir}" && hgcc -Wall -o "${outFileName}" "./${fileName}" && "./${outFileName}"`
+    const mainTerminal =
+      vscode.window.terminals.find((t) => t.name === terminalNames.main) ||
+      vscode.window.createTerminal(terminalNames.main);
+    mainTerminal.sendText(
+      `cd "${dirAbsPath}" && hgcc -Wall -o "${outFileName}" "./${fileName}" && "./${outFileName}"`
     );
 
     if (config.get("showTerminal")) {
-      terminal.show();
+      mainTerminal.show();
     }
   });
 
